eeda57cd3e0c1fca7892a89d1c07ea2305fe9cd9
Julien BASQUIN <jbasquin@iorga.com>
2020-10-05 19:14:28 +0200

    formLogin.php + routing.php + contactForm.php + create dir poo

--- formLogin.php
+++ formLogin.php
@@ -1,14 +1,13 @@
 <?php
-//bdd
-$users = ["julien" => "pass", "coderKid" => "pa55w0rd*", "dogWalker" => "ais1eofdog$"];
+$users = ["coolBro123" => "password123!", "coderKid" => "pa55w0rd*", "dogWalker" => "ais1eofdog$"];
 
-// messages callback
+
 $feedback = "";
 $message = "You're logged in!";
 $validation_error = "* Incorrect username or password.";
 $username = "";
 
-// traitement
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
@@ -20,18 +19,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 ?>
-<!--form login-->
-<h3>Form login</h3>
+
+<h3>Welcome back!</h3>
 <form method="post" action="">
-  <label>
-      Username:
-        <input type="text" name="username" value="<?php echo $username;?>">
-    </label>
+    Username:<input type="text" name="username" value="<?php echo $username;?>">
     <br>
-   <label>
-       Password:
-        <input type="text" name="password" value="">
-    </label>
+    Password:<input type="text" name="password" value="">
     <br>
     <input type="submit" value="Log in">
 </form>
